a952a46a0e7f0525cd34941526f3d1c016b67d77
Joseph Young <joseph.young.dev@gmail.com>
2020-12-22 23:59:36 -0500

    added basic component for directory

---
+++ client/src/components/HelloWorld/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelloWorld () {
+    return (
+        <div>
+            Hello World from HelloWorld component 
+        </div>
+    )
+}
+
+export default HelloWorld;
--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HelloWorld from '../../components/HelloWorld'
 
 function Home () {
     return (
         <div>
-            Hello World from Homepage 
+            <HelloWorld />
         </div>
     )
 }
